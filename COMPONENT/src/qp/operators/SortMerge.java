4ac7fff42ec5ef9b56c80511fb3441df6aad391d
Boon Jun <boon-jun1996@hotmail.com>
2020-03-26 20:35:30 +0800

    SortMerge: Implement single pass merge on sorted runs

--- COMPONENT/src/qp/operators/SortMerge.java
+++ COMPONENT/src/qp/operators/SortMerge.java
@@ -2,8 +2,11 @@ package qp.operators;
 
 import qp.utils.*;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class SortMerge {
     private boolean reverse;
@@ -22,6 +25,13 @@ public class SortMerge {
         this.reverse = reverse;
         int tupleSize = base.getSchema().getTupleSize();
         this.batchSize = Batch.getPageSize()/tupleSize;
+    }
+
+    private void deleteTempFiles() {
+        for (int x = 0; x < this.tempFiles.size(); x++) {
+            File f = new File(this.tempFiles.get(x));
+            f.delete();
+        }
     }
 
     private void writeTuplesArrayOutput(ArrayList<Tuple> tuples, String fileName) {
@@ -73,5 +83,78 @@ public class SortMerge {
 
         this.pass++;
         this.base.close();
+    }
+
+    private void merge(ArrayList<TupleReader> readers, String output_file) {
+        TupleWriter writer = new TupleWriter(output_file, this.batchSize);
+        Comparator<TupleIndexPair> custom_comparator = new Comparator<TupleIndexPair>() {
+            @Override
+            public int compare(TupleIndexPair o1, TupleIndexPair o2) {
+                int result = Tuple.compareTuples(o1.tuple, o2.tuple, compareIndex, compareIndex);
+                return reverse ? -result: result;
+            }
+        };
+
+        PriorityQueue<TupleIndexPair> pq = new PriorityQueue<>(numBuffer - 1, custom_comparator);
+
+        if (!writer.open()) {
+            System.out.printf("%s:writing merged file error", output_file);
+            System.exit(1);
+        }
+
+        //Populate PQ with first tuple from each buffer
+        for (int x = 0; x < readers.size(); x++) {
+            Tuple tuple = readers.get(x).next();
+            assert(tuple != null);
+            pq.add(new TupleIndexPair(tuple, x));
+        }
+
+        //Merge and place each element into the output buffer
+        while (!pq.isEmpty()) {
+            TupleIndexPair tupleIndexPair = pq.poll();
+            writer.next(tupleIndexPair.tuple);
+            Tuple nextTuple = readers.get(tupleIndexPair.index).next();
+            if (nextTuple != null) {
+                pq.add(new TupleIndexPair(nextTuple, tupleIndexPair.index));
+            }
+        }
+        writer.close();
+    }
+
+    private void mergeRuns() {
+        ArrayList<String> next_sorted_temp_runs = new ArrayList<>();
+        ArrayList<TupleReader> readers = new ArrayList<>();
+        int run_count = 0;
+        int x = 0;
+
+        while (x < this.tempFiles.size()) {
+            do {
+                String tempFileName = this.tempFiles.get(x);
+                TupleReader reader = new TupleReader(tempFileName, this.batchSize);
+                if (!reader.open()) {
+                    System.out.printf("%s: Unable to open file to during merging", tempFileName);
+                    System.exit(1);
+                }
+                readers.add(reader);
+                x++;
+            } while (x < this.tempFiles.size() && x % (this.batchSize - 1) != 0);
+            String output_file = "SMtempRun-" + pass + "-" + run_count;
+            merge(readers, output_file);
+            next_sorted_temp_runs.add(output_file);
+            run_count++;
+        }
+        this.pass++;
+        deleteTempFiles();
+        this.tempFiles = next_sorted_temp_runs;
+    }
+
+    private class TupleIndexPair {
+        Tuple tuple;
+        int index;
+
+        private TupleIndexPair(Tuple tuple, int index) {
+            this.tuple = tuple;
+            this.index = index;
+        }
     }
 }
