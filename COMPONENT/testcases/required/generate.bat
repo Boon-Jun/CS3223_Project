32384605be91aca2f224679aeb0b9209a45c83f0
wm28 <weiming199628@gmail.com>
2020-04-06 21:54:17 +0800

    Add all relevant queries & db files for experiments

---
+++ COMPONENT/testcases/required/generate.bat
@@ -0,0 +1,11 @@
+java RandomDB AIRCRAFT 10000
+java RandomDB CERTIFIED 10000
+java RandomDB EMPLOYEES 10000
+java RandomDB FLIGHTS 10000
+java RandomDB SCHEDULE 10000
+
+java ConvertTxtToTbl AIRCRAFT
+java ConvertTxtToTbl CERTIFIED
+java ConvertTxtToTbl EMPLOYEES
+java ConvertTxtToTbl FLIGHTS
+java ConvertTxtToTbl SCHEDULE
